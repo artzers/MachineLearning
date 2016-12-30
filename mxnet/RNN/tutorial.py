b654e3ba7f5c1d86d6fcd6acfea7a7775f56c950
artzers <263699527@qq.com>
2016-12-31 02:33:56 +0800

    test run, failed

--- mxnet/RNN/tutorial.py
+++ mxnet/RNN/tutorial.py
@@ -1,22 +1,31 @@
-import os
-import urllib
-import zipfile
+#-*-coding:utf-8-*-
+import mxnet as mx
 import numpy as np
-import random, bisect
-# if not os.path.exists("char_lstm.zip"):
-#     urllib.urlretrieve("http://data.mxnet.io/data/char_lstm.zip", "char_lstm.zip")
-# with zipfile.ZipFile("char_lstm.zip","r") as f:
-#     f.extractall("./")
-with open('obama.txt', 'r') as f:
-    print f.read()[0:1000]
-
+import codecs
+import random
+import bisect
+# set up logging
+import logging
+reload(logging)
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG, datefmt='%I:%M:%S')
+from lstm import lstm_unroll, lstm_inference_symbol
+from bucket_io import BucketSentenceIter
+from rnn_model import LSTMInferenceModel
+# Read from doc
 def read_content(path):
     with open(path) as ins:
-        return ins.read()
+        content = ins.read()
+        return content
 
-# Return a dict which maps each char into an unique int id
+def read_chinese_content(path):
+    with codecs.open(path,encoding='utf-8') as ins:
+        content = ins.read()
+        return content
+
+# Build a vocabulary of what char we have in the content
 def build_vocab(path):
-    content = list(read_content(path))
+    content = read_content(path)
+    content = list(content)
     idx = 1 # 0 is left for zero-padding
     the_vocab = {}
     for word in content:
@@ -27,107 +36,104 @@ def build_vocab(path):
             idx += 1
     return the_vocab
 
-# Encode a sentence with int ids
+def build_chinese_vocab(path):
+    content = read_chinese_content(path)
+    content = list(content)
+    idx = 1 # 0 is left for zero-padding
+    the_vocab = {}
+    for word in content:
+        if len(word) == 0 or word == "\n":
+            continue
+        if not word in the_vocab:
+            the_vocab[word] = idx
+            idx += 1
+    return the_vocab
+
+# We will assign each char with a special numerical id
 def text2id(sentence, the_vocab):
     words = list(sentence)
-    return [the_vocab[w] for w in words if len(w) > 0]
+    words = [the_vocab[w] for w in words if len(w) > 0]
+    return words
 
-# build char vocabluary from input
-vocab = build_vocab("./obama.txt")
-print('vocab size = %d' %(len(vocab)))
 
-"""test_utility_function"""
-data_file = open("./obama.txt")
-vocab_key_list = vocab.keys()
-validate_set = set(data_file.read())
-assert len(vocab_key_list) == len(validate_set), "Vocabulary dictionary key set not correct."
-for key in vocab_key_list:
-    assert key in validate_set, "Vocabulary dictionary key set not correct."
-data_file.close()
+# Evaluation
+def Perplexity(label, pred):
+    label = label.T.reshape((-1,))
+    loss = 0.
+    for i in range(pred.shape[0]):
+        loss += -np.log(max(1e-10, pred[i][int(label[i])]))
+    #print np.exp(loss / label.size)
+    return np.exp(loss / label.size)
 
-import lstm
-# Each line contains at most 129 chars.
-seq_len = 150
-# embedding dimension, which maps a character to a 256-dimension vector
-num_embed = 256
-# number of lstm layers
-num_lstm_layer = 3
-# hidden unit in LSTM cell
-num_hidden = 512
+def lr_callback(epoch, symbol, arg_params, aux_params):
+    print 'epoch: ',epoch
 
-symbol = lstm.lstm_unroll(
-    num_lstm_layer,
-    seq_len,
-    len(vocab) + 1,
-    num_hidden=num_hidden,
-    num_embed=num_embed,
-    num_label=len(vocab) + 1,
-    dropout=0.2)
-
-"""test_seq_len"""
-data_file = open("./obama.txt")
-for line in data_file:
-    assert len(line) <= seq_len + 1, "seq_len is smaller than maximum line length. Current line length is %d. Line content is: %s" % (len(line), line)
-data_file.close()
-
-import bucket_io
+# import os
+# data_url = "http://data.mxnet.io/mxnet/data/char_lstm.zip"
+# os.system("wget %s" % data_url)
+# os.system("unzip -o char_lstm.zip")
 
 # The batch size for training
 batch_size = 32
+# We can support various length input
+# For this problem, we cut each input sentence to length of 129
+# So we only need fix length bucket
+buckets = [60]
+# hidden unit in LSTM cell
+num_hidden = 512
+# embedding dimension, which is, map a char to a 256 dim vector
+num_embed = 256
+# number of lstm layer
+num_lstm_layer = 3
+
+# we will show a quick demo in 2 epoch
+# and we will see result by training 75 epoch
+num_epoch = 30
+# learning rate
+learning_rate = 0.01
+# we will use pure sgd without momentum
+momentum = 0.0
+
+filePath = "./shige1.txt"#"./obama.txt"
+# build char vocabluary from input
+vocab = build_chinese_vocab(filePath)
+#vocab = build_chinese_vocab(filePath)
+
+
+# generate symbol for a length
+def sym_gen(seq_len):
+    return lstm_unroll(num_lstm_layer, seq_len, len(vocab) + 1,
+                       num_hidden=num_hidden, num_embed=num_embed,
+                       num_label=len(vocab) + 1, dropout=0.2)
 
 # initalize states for LSTM
 init_c = [('l%d_init_c'%l, (batch_size, num_hidden)) for l in range(num_lstm_layer)]
 init_h = [('l%d_init_h'%l, (batch_size, num_hidden)) for l in range(num_lstm_layer)]
 init_states = init_c + init_h
 
-# Even though BucketSentenceIter supports various length examples,
-# we simply use the fixed length version here
-data_train = bucket_io.BucketSentenceIter(
-    "./obama.txt",
-    vocab,
-    [seq_len],
-    batch_size,
-    init_states,
-    seperate_char='\n',
-    text2id=text2id,
-    read_content=read_content)
+# we can build an iterator for text
+data_train = BucketSentenceIter(filePath, vocab, buckets, batch_size,
+                                init_states, seperate_char='\n',
+                                text2id=text2id, read_content=read_chinese_content)#read_content)
 
-# Output may vary
-import mxnet as mx
-import numpy as np
-import logging
-logging.getLogger().setLevel(logging.DEBUG)
+# the network symbol
+symbol = sym_gen(buckets[0])
+# Train a LSTM network as simple as feedforward network
+model = mx.model.FeedForward(ctx=mx.cpu(),
+                             symbol=symbol,
+                             num_epoch=num_epoch,
+                             learning_rate=learning_rate,
+                             momentum=momentum,
+                             wd=0.0001,
+                             initializer=mx.init.Xavier(factor_type="in", magnitude=2.34))
 
-# We will show a quick demo with only 1 epoch. In practice, we can set it to be 100
-num_epoch = 75
-# learning rate
-learning_rate = 0.01
-
-# Evaluation metric
-def Perplexity(label, pred):
-    loss = 0.
-    for i in range(pred.shape[0]):
-        loss += -np.log(max(1e-10, pred[i][int(label[i])]))
-    return np.exp(loss / label.size)
-
-model = mx.model.FeedForward(
-    ctx=mx.gpu(0),
-    symbol=symbol,
-    num_epoch=num_epoch,
-    learning_rate=learning_rate,
-    momentum=0,
-    wd=0.0001,
-    initializer=mx.init.Xavier(factor_type="in", magnitude=2.34))
-
+# Fit it
 model.fit(X=data_train,
-          eval_metric=mx.metric.np(Perplexity),
-          batch_end_callback=mx.callback.Speedometer(batch_size, 20),
-          )#epoch_end_callback=mx.callback.do_checkpoint("obama")
+          eval_metric = mx.metric.np(Perplexity),
+          batch_end_callback=mx.callback.Speedometer(batch_size, 50),
+          epoch_end_callback=lr_callback)#mx.callback.do_checkpoint("obama"))
 
-model.save('obama')
-
-from rnn_model import LSTMInferenceModel
-
+model.save('tutorial')
 
 # helper strcuture for prediction
 def MakeRevertVocab(vocab):
@@ -174,30 +180,23 @@ def MakeOutput(prob, vocab, sample=False, temperature=1.):
         char = vocab[idx]
     except:
         char = ''
-    print char
     return char
 
-import rnn_model
-
 # load from check-point
-_, arg_params, __ = mx.model.load_checkpoint("obama", 75)
+_, arg_params, __ = mx.model.load_checkpoint("tutorial", num_epoch)
 
 # build an inference model
-model = rnn_model.LSTMInferenceModel(
-    num_lstm_layer,
-    len(vocab) + 1,
-    num_hidden=num_hidden,
-    num_embed=num_embed,
-    num_label=len(vocab) + 1,
-    arg_params=arg_params,
-    ctx=mx.gpu(),
-    dropout=0.2)
+model = LSTMInferenceModel(num_lstm_layer, len(vocab) + 1,
+                           num_hidden=num_hidden, num_embed=num_embed,
+                           num_label=len(vocab) + 1, arg_params=arg_params, ctx=mx.cpu(), dropout=0.2)
+
+# generate a sequence of 1200 chars
 
 seq_length = 20
 input_ndarray = mx.nd.zeros((1,))
 revert_vocab = MakeRevertVocab(vocab)
 # Feel free to change the starter sentence
-output ='The'
+output =revert_vocab[np.random.randint(1,len(revert_vocab)-1)]#'The joke'#
 random_sample = False
 new_sentence = True
 
@@ -215,4 +214,5 @@ for i in range(seq_length):
         new_sentence = True
     if i >= ignore_length - 1:
         output += next_char
+
 print output
--- mxnet/RNN/tutorial_generate.py
+++ mxnet/RNN/tutorial_generate.py
@@ -1,34 +1,28 @@
-#-*-coding:utf-8-*-
-import os
-import codecs
-# import urllib
-# import zipfile
-import sys
-default_encoding="utf-8"
-if(default_encoding!=sys.getdefaultencoding()):
-    reload(sys)
-    sys.setdefaultencoding(default_encoding)
-
-# if not os.path.exists("char_lstm.zip"):
-#     urllib.urlretrieve("http://data.mxnet.io/data/char_lstm.zip", "char_lstm.zip")
-#
-# with zipfile.ZipFile("char_lstm.zip","r") as f:
-#     f.extractall("./")
-
-# with open('hehe.txt', 'r') as f:
-#     print f.read()[0:1000]
-
-filePath = './obama.txt'
-
+#coding=gbk
+#coding=utf-8
+#-*- coding: UTF-8 -*-
 import mxnet as mx
-import numpy as np, random, bisect
+import numpy as np
+import codecs
+import random
+import bisect
+# set up logging
+import logging
+reload(logging)
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG, datefmt='%I:%M:%S')
 from lstm import lstm_unroll, lstm_inference_symbol
 from bucket_io import BucketSentenceIter
 from rnn_model import LSTMInferenceModel
-
+# Read from doc
 def read_content(path):
-    with codecs.open(path,'r','utf-8')as ins:
-        return ins.read()[1:]
+    with open(path) as ins:
+        content = ins.read()
+        return content
+
+def read_chinese_content(path):
+    with codecs.open(path,encoding='utf-8') as ins:
+        content = ins.read()
+        return content
 
 # Build a vocabulary of what char we have in the content
 def build_vocab(path):
@@ -37,7 +31,20 @@ def build_vocab(path):
     idx = 1 # 0 is left for zero-padding
     the_vocab = {}
     for word in content:
-        if len(word) == 0 or word == '\r' or word == '\n':
+        if len(word) == 0:
+            continue
+        if not word in the_vocab:
+            the_vocab[word] = idx
+            idx += 1
+    return the_vocab
+
+def build_chinese_vocab(path):
+    content = read_chinese_content(path)
+    content = list(content)
+    idx = 1 # 0 is left for zero-padding
+    the_vocab = {}
+    for word in content:
+        if len(word) == 0 or word == "\n":
             continue
         if not word in the_vocab:
             the_vocab[word] = idx
@@ -50,6 +57,7 @@ def text2id(sentence, the_vocab):
     words = [the_vocab[w] for w in words if len(w) > 0]
     return words
 
+
 # Evaluation
 def Perplexity(label, pred):
     label = label.T.reshape((-1,))
@@ -58,12 +66,17 @@ def Perplexity(label, pred):
         loss += -np.log(max(1e-10, pred[i][int(label[i])]))
     return np.exp(loss / label.size)
 
+# import os
+# data_url = "http://data.mxnet.io/mxnet/data/char_lstm.zip"
+# os.system("wget %s" % data_url)
+# os.system("unzip -o char_lstm.zip")
+
 # The batch size for training
-batch_size = 16
+batch_size = 32
 # We can support various length input
 # For this problem, we cut each input sentence to length of 129
 # So we only need fix length bucket
-#buckets = [100]
+buckets = [60]
 # hidden unit in LSTM cell
 num_hidden = 512
 # embedding dimension, which is, map a char to a 256 dim vector
@@ -74,15 +87,16 @@ num_lstm_layer = 3
 # we will show a quick demo in 2 epoch
 # and we will see result by training 75 epoch
 num_epoch = 30
+# learning rate
+learning_rate = 0.01
 # we will use pure sgd without momentum
 momentum = 0.0
 
-# we can select multi-gpu for training
-# for this demo we only use one
-devs = [mx.context.gpu(i) for i in range(1)]
-
+filePath = "./shige1.txt"#"./obama.txt"
 # build char vocabluary from input
-vocab = build_vocab(filePath)
+#vocab = build_vocab(filePath)
+vocab = build_chinese_vocab(filePath)
+
 
 # helper strcuture for prediction
 def MakeRevertVocab(vocab):
@@ -120,7 +134,7 @@ def MakeOutput(prob, vocab, sample=False, temperature=1.):
     if sample == False:
         idx = np.argmax(prob, axis=1)[0]
     else:
-        fix_dict = [""] + [vocab[i] for i in range(1, len(vocab)+1 )]
+        fix_dict = [""] + [vocab[i] for i in range(1, len(vocab) + 1)]
         scale_prob = np.clip(prob, 1e-6, 1 - 1e-6)
         rescale = np.exp(np.log(scale_prob) / temperature)
         rescale[:] /= rescale.sum()
@@ -132,25 +146,25 @@ def MakeOutput(prob, vocab, sample=False, temperature=1.):
     return char
 
 # load from check-point
-_, arg_params, __ = mx.model.load_checkpoint("hehe", num_epoch)
+_, arg_params, __ = mx.model.load_checkpoint("tutorial", num_epoch)
 
 # build an inference model
-model = LSTMInferenceModel(num_lstm_layer, len(vocab)+1,
+model = LSTMInferenceModel(num_lstm_layer, len(vocab) + 1,
                            num_hidden=num_hidden, num_embed=num_embed,
-                           num_label=len(vocab)+1, arg_params=arg_params, ctx=mx.gpu(), dropout=0.2)
+                           num_label=len(vocab) + 1, arg_params=arg_params, ctx=mx.cpu(), dropout=0.2)
 
 # generate a sequence of 1200 chars
 
-seq_length = 20
+seq_length = 100
 input_ndarray = mx.nd.zeros((1,))
 revert_vocab = MakeRevertVocab(vocab)
 # Feel free to change the starter sentence
-output ='The joke'#revert_vocab[np.random.randint(4, len(vocab)-1)]
-random_sample = False#True
+output =revert_vocab[np.random.randint(1,len(revert_vocab)-1)]#'The joke'#
+random_sample = False
 new_sentence = True
 
 ignore_length = len(output)
-cache = output[0]
+
 for i in range(seq_length):
     if i <= ignore_length - 1:
         MakeInput(output[i], vocab, input_ndarray)
@@ -163,34 +177,5 @@ for i in range(seq_length):
         new_sentence = True
     if i >= ignore_length - 1:
         output += next_char
-# for i in range(seq_length):
-#     if i % 5 == 0:
-#         new_sentence = True
-#         if i > 0:
-#            output+=' '
-#     else:
-#         new_sentence = False
-#     if i <= ignore_length - 1:
-#         MakeInput(cache[i], vocab, input_ndarray)
-#     else:
-#         MakeInput(cache[-1], vocab, input_ndarray)
-#     prob = model.forward(input_ndarray, new_sentence)
-#     next_char = MakeOutput(prob, revert_vocab, random_sample)
-#     while next_char == '':
-#         new_sentence = True
-#         if i <= ignore_length - 1:
-#             MakeInput(cache[i], vocab, input_ndarray)
-#         else:
-#             MakeInput(cache[-1], vocab, input_ndarray)
-#         prob = model.forward(input_ndarray, new_sentence)
-#         #new_sentence = False
-#         next_char = MakeOutput(prob, revert_vocab, random_sample)
-#
-#     if i >= ignore_length - 1:
-#         #if i == 0:
-#         #    output = next_char
-#         #else:
-#         output += next_char
-#         cache += next_char
 
 print output
